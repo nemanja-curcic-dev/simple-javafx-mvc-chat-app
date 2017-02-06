9478f3955e96750f4693102fb2003343ac40c050
nemanja-curcic-dev <curesr@gmail.com>
2017-02-06 18:18:42 +0100

    second commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,28 @@
 package app;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by nemanja on 6.2.17..
  */
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/window.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
